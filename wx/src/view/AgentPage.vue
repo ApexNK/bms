10442e0f323fdafe31134ed6455023f23c0dbef1
vidy <120648590@qq.com>
2017-04-24 20:13:58 +0800

    #agentPage

---
+++ wx/src/view/AgentPage.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="loading-box">
+
+      获取用户信息中......
+
+  </div>
+</template>
+<script>
+  import { WECHAT_OPENID } from '../config.js';
+  export default {
+    data () {
+      return {
+      };
+    },
+
+    mounted () {
+      this._http.get('user/detail').then(res => {
+        if (res.code === 6) {
+          window.location.href = WECHAT_OPENID;
+          return Promise.resolve(res);
+        }
+        this.$router.replace({name: 'Tab'});
+        return Promise.reject();
+      });
+    },
+    destroyed () {
+
+    },
+    methods: {
+    }
+  }
+</script>
+<style lang="scss" scoped>
+  .loading-box{
+    padding-top: 100px;
+    text-align: center;
+    color: #fff;
+  }
+</style>
