3d61906b698e7b1eca6f56b90fdd6c70475c8517
huangww <huangweiwei@youbangsoft.com>
2017-04-25 02:25:17 +0800

    添加个人中心接口

--- wx/src/view/TabPage/Income.vue
+++ wx/src/view/TabPage/Income.vue
@@ -2,35 +2,35 @@
   <Scroller>
     <div class="card dashboard">
       <div class="text-center">总收益(元)</div>
-      <h2 class="text-center warn-color">4000.00</h2>
+      <h2 class="text-center warn-color">{{toFixed(memberInfo.totalRevenue)}}</h2>
       <div class="total-bar row row-no-padding">
         <div class="col">
           今日收益<br/>
-          <span class="warn-color">+5846.00</span>
+          <span class="warn-color">{{memberInfo.dailyRevenue > -0.0001 ? ("+" + toFixed(memberInfo.dailyRevenue)) : ("-" + toFixed(memberInfo.dailyRevenue))}}</span>
           </div>
         <div class="col">
           当月累计收益<br/>
-          <span class="warn-color">+5846.00</span>
+          <span class="warn-color">+{{toFixed(memberInfo.monthlyRevenue)}}</span>
           </div>
-        <div class="col">业绩奖励<br/><span class="warn-color">+30000</span></div>
+        <div class="col">业绩奖励<br/><span class="warn-color">+{{toFixed(memberInfo.bonus)}}</span></div>
       </div>
     </div>
     <ul class="list">
       <li class="item">
         <span class="">投资收益率:</span>
-        <span class="fr main-color">9.9</span>
+        <span class="fr main-color">{{toFixed(memberInfo.revenueRate*100)}}%</span>
       </li>
       <li class="item">
         <span class="">积分:</span>
-        <span class="fr warn-color">+3000.00 </span>
+        <span class="fr warn-color">+{{memberInfo.score}}</span>
       </li>
       <li class="item">
         <span class="">盈亏提现:</span>
-        <span class="fr warn-color">+3000.00 </span>
+        <span class="fr warn-color">+{{toFixed(memberInfo.withdraw)}} </span>
       </li>
       <li class="item">
         <span class="">总提现金额:</span>
-        <span class="fr warn-color">9000.00</span>
+        <span class="fr warn-color">{{toFixed(memberInfo.totalWithdraw)}}</span>
       </li>
     </ul>
   </Scroller>
@@ -46,16 +46,43 @@
       },
       data () {
         return {
-          curState: 'userCenter'
+          curState: 'userCenter',
+          memberInfo: {
+            monthlyRevenue:0,
+            dailyRevenue: 0,
+            revenueRate: 0,//收益率
+            score:0,
+            totalWithdraw:0,
+            totalRevenue:0, //总收入
+           // winLose: 0, //盈亏
+            withdraw: 0,//盈亏提现
+            bonus: 0 //业绩奖励
+          }
         }
       },
       mounted () {
-        this._http.get("user/detail").then(res => {
+        var self = this;
+        this._http.get("user/revenue").then(res => {
           console.info(res);
+          if(res.code !== 0){
+            console.info(res.message);
+            return;
+          }
+          for(let k in self.memberInfo) {
+            self.memberInfo[k] = res.value[k];
+          }
         });
       },
       methods: {
-
+        toFixed(value){
+          var temp = value + "";
+          let index  = temp.indexOf(".");
+          if(index < 0 ) {
+            return (value + ".00");
+          }
+          let str = temp.substring(0,temp.indexOf(".") + 3);
+          return str;
+        }
       }
     }
 </script>
--- wx/src/view/TabPage/UserCenter.vue
+++ wx/src/view/TabPage/UserCenter.vue
@@ -2,30 +2,28 @@
   <Scroller>
     <div class="card">
       <div class="row row-no-padding row-wrap">
-        <div class="col-50">账号：900001</div>
-        <div class="col-50">姓名：吕杰</div>
+        <div class="col-50">账号：{{member.loginName}}</div>
+        <div class="col-50">姓名：{{member.name}}</div>
         <div class="col-50">代码：690891</div>
         <div class="col-50">名称：恒玖盈</div>
-        <div class="col-50">类型：劣后级</div>
-        <div class="col-50">数量：2000万</div>
-        <div class="col-50">成本：0.5</div>
-        <div class="col-50">买入日期：2017.1.1</div>
+        <div class="col-50">类型：{{member.grand ? "优先级" : "劣后级"}}</div>
+        <div class="col-50">数量：{{member.quantity}}</div>
+        <div class="col-50">成本：{{toFixed(member.cost)}}元</div>
+        <div class="col-50">买入日期：{{filterDate(member.purchaseDate)}}</div>
 
       </div>
     </div>
     <div class="card">
       <div class="row row-no-padding row-wrap">
-        <div class="col-50">最新价格：<span class="warn-color">900001</span></div>
-        <div class="col-50">当前价值：<span class="main-color">9000万元</span></div>
-        <div class="col-50">盈亏：<span class="warn-color">900001</span></div>
-        <div class="col-50">盈亏比例：<span class="warn-color">401%</span></div>
-        <div class="col-50">回购日期：<span class="main-color">900001</span></div>
-        <div class="col-50">最低回购价：<span class="main-color">900001</span></div>
+        <div class="col-50">最新价格：<span class="warn-color">{{toFixed(member.currentPrice)}}</span></div>
+        <div class="col-50">当前价值：<span class="main-color">{{toFixed(revent.curentValue)}}元</span></div>
+        <div class="col-50">盈亏：<span class="warn-color">{{toFixed(revent.winLose)}}</span></div>
+        <div class="col-50">盈亏比例：<span class="warn-color">{{toFixed(revent.winLoseRate * 100)}}%</span></div>
+        <div class="col-50">回购日期：<span class="main-color">{{filterDate(member.buybackDate)}}</span></div>
+        <div class="col-50">最低回购价：<span class="main-color">{{toFixed(member.buybackPrice)}}元</span></div>
 
       </div>
     </div>
-
-
 
 
   </Scroller>
@@ -41,16 +39,65 @@
       },
       data () {
         return {
-          curState: 'userCenter'
+          curState: 'userCenter',
+          member: {
+            name:"",
+            loginName:"",
+            grand: 0,
+            quantity: 0,
+            cost: 0,
+            purchaseDate: "",
+            currentPrice: 0,
+            buybackPrice: 0,
+            buybackDate: "",
+            withdraw: 0
+          },
+          revent: {
+            winLose: 0,
+            winLoseRate: 0,
+            curentValue: 0
+          }
         }
       },
       mounted () {
-        this._http.get("user/revenue").then(res => {
+        var self = this;
+        this._http.get("user/detail").then(res => {
           console.info(res);
+          if(res.code !== 0){
+            console.info(res.message);
+            return;
+          }
+          for(let k in self.member) {
+            self.member[k] = res.value[k];
+          }
         });
+        this._http.get("user/revenue").then(res => {
+        if(res.code !== 0){
+          console.info(res.message);
+          return;
+        }
+        for(let k in self.revent) {
+          self.revent[k] = res.value[k];
+        }
+      });
       },
       methods: {
-
+        toFixed(value){
+          var temp = value + "";
+          let index  = temp.indexOf(".");
+          if(index < 0 ) {
+            return (value + ".00");
+          }
+          let str = temp.substring(0,temp.indexOf(".") + 3);
+          return str;
+        },
+        filterDate(value){
+          var date = new Date(value);
+          let Y = date.getFullYear() + '-';
+          let M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-';
+          let D = date.getDate();
+          return (Y+M+D);
+        }
       }
     }
 </script>
