4587b04e8924142eb5214ff1ed74992e8596c70e
vidy <120648590@qq.com>
2017-04-24 22:26:25 +0800

    #配置优化

--- wx/src/config.js
+++ wx/src/config.js
@@ -1,10 +1,10 @@
 // 本地配置信息
-const appId = 'wxd2124dd20a541894';
-const urlName = 'hjy.tunnel.2bdata.com';
-const SiteURL = 'http://hjy.tunnel.2bdata.com/';
-//const WECHAT_OPENID = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx9a42b6525768c05b&redirect_uri=http%3a%2f%vidy.tunnel.2bdata.com%2fwechat%2fgrant&response_type=code&scope=snsapi_base&state=123#wechat_redirect`;
-const WECHAT_OPENID = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxd2124dd20a541894&redirect_uri=http%3a%2f%2fhjy.tunnel.2bdata.com%2fwechat%2fgrant&response_type=code&scope=snsapi_base&state=123#wechat_redirect`;
-//const WECHAT_OPENID = ' https://open.weixin.qq.com/connect/oauth2/authorize?appid='+ appId +'&redirect_uri=http%3a%2f%2f' + urlName + '%2fwechat%2fgrant&response_type=code&scope=snsapi_base&state=123#wechat_redirect`;
+const appId = 'wx9c351d8ab91d4f26';
+const urlName = 'vidy.tunnel.2bdata.com';
+const SiteURL = 'http://vidy.tunnel.2bdata.com/';
+// const WECHAT_OPENID = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx9a42b6525768c05b&redirect_uri=http%3a%2f%vidy.tunnel.2bdata.com%2fwechat%2fgrant&response_type=code&scope=snsapi_base&state=123#wechat_redirect`;
+const WECHAT_OPENID = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${appId}&redirect_uri=http%3a%2f%2f${urlName}%2fwechat%2fgrant&response_type=code&scope=snsapi_base&state=123#wechat_redirect`;
+// const WECHAT_OPENID = ' https://open.weixin.qq.com/connect/oauth2/authorize?appid='+ appId +'&redirect_uri=http%3a%2f%2f' + urlName + '%2fwechat%2fgrant&response_type=code&scope=snsapi_base&state=123#wechat_redirect`;
 const TitleName = 'TOEFL';
 export default {
   SiteURL,
