afbf04a7056b278b4c8c68685e40e4f8ebe17ad2
huangww <huangweiwei@youbangsoft.com>
2017-04-26 00:19:45 +0800

    修改时间过滤器和第一次跳转

--- wx/src/view/AgentPage.vue
+++ wx/src/view/AgentPage.vue
@@ -14,15 +14,7 @@
     },
 
     mounted () {
-//      this._http.get('user/detail').then(res => {
-//        if (res.code === 6) {
-//          window.location.href = WECHAT_OPENID;
-//          return Promise.resolve(res);
-//        }
-//        this.$router.replace({name: 'Tab'});
-//        return Promise.reject();
-//      });
-      // window.location.href = WECHAT_OPENID;
+       window.location.href = WECHAT_OPENID;
       // location.href = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxd2124dd20a541894&redirect_uri=http%3a%2f%2fhjy.tunnel.2bdata.com%2fwechat%2fgrant&response_type=code&scope=snsapi_base&state=123#wechat_redirect";
     },
     destroyed () {
--- wx/src/view/TabPage/UserCenter.vue
+++ wx/src/view/TabPage/UserCenter.vue
@@ -93,9 +93,12 @@
           return str;
         },
         filterDate(value){
+          if(!value){
+            return "";
+          }
           var date = new Date(value);
-          let Y = date.getFullYear() + '-';
-          let M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-';
+          let Y = date.getFullYear() + '.';
+          let M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '.';
           let D = date.getDate();
           return (Y+M+D);
         }
